efedc99549c921e8588d80e9be973b69760a4f5d
ajenkinski <adam@thejenkins.org>
2022-10-15 21:02:22 -0400

    Added missing check in square generator for valid square.

--- app/Main.hs
+++ app/Main.hs
@@ -18,7 +18,7 @@ main = do
   let squares = Enumerate5x5.allSquares
   let validate square = sum (Enumerate5x5.allSquareValues square) == 325
   -- print an update every updateEvery squares so I know it's making progress
-  let updateEvery = 10000
+  let updateEvery = 1000
   printf "Starting computation. Printing an update every %v squares.\n" (formatInt updateEvery)
   SIO.hSetBuffering SIO.stdout SIO.NoBuffering
   -- The call to seq in the foldM function is necessary to make squares be solved
@@ -26,8 +26,8 @@ main = do
   -- the squares don't actually get solved until counts get printed out.
   (numValid, totalCount) <- foldM (\(n,t) square ->
                                   do when (seq n False) (error "Force eval")
-                                     when (t `mod` updateEvery == 0) (printf "%15v squares found\r" (formatInt t))
-                                     return (if validate square then n+1 else n,
+                                     when (t `mod` updateEvery == 0) (printf "%15v squares found, %v valid\r" (formatInt t) (formatInt n))
+                                     return (if Enumerate5x5.squareIsValid square then n+1 else n,
                                              t + 1))
                        (0, 0) squares
   printf "\ntotalCount = %v, numValid = %v\n" (formatInt totalCount) (formatInt numValid)
--- src/Enumerate5x5.hs
+++ src/Enumerate5x5.hs
@@ -1,53 +1,58 @@
 module Enumerate5x5 where
 
+import Control.Monad (guard, join)
+import Data.Array ((!))
+import qualified Data.Array as Array
+import qualified Data.IntSet as Set
 import Data.List ((\\))
 import qualified Data.List as List
 import qualified Data.Map.Strict as Map
-import qualified Data.IntSet as Set
-import qualified Data.Array as Array
-import Data.Array ((!))
 import qualified Data.Maybe as Maybe
 import Text.Printf (printf)
-import Control.Monad (guard)
 
 -- Return all "subsets of length k" of xs
 ssolk :: Int -> [a] -> [[a]]
 ssolk k xs
-    | k == 0    = [[]]
-    | otherwise =
-        [x:ss | (x:rest) <- List.tails xs, ss <- ssolk (k-1) rest]
+  | k == 0 = [[]]
+  | otherwise =
+      [x : ss | (x : rest) <- List.tails xs, ss <- ssolk (k - 1) rest]
 
 type ComponentVector = [Int]
 
 type Idx = (Int, Int)
+
 type Square = Array.Array Idx Int
 
-data Component = 
-    Row Int |
-    Col Int |
-    MainDiag |  -- top left to bottom right
-    MinorDiag   -- top right to bottom left
-
+data Component
+  = Row Int
+  | Col Int
+  | MainDiag -- top left to bottom right
+  | MinorDiag -- top right to bottom left
 
 componentIndices :: Component -> [Idx]
 componentIndices comp =
-    case comp of
-        Row r -> [(r, c) | c <- [0..4]]
-        Col c -> [(r, c) | r <- [0..4]]
-        MainDiag -> [(i, i) | i <- [0..4]]
-        MinorDiag -> [(r, c) | (r, c) <- zip [0..4] [4,3..0]]
+  case comp of
+    Row r -> [(r, c) | c <- [0 .. 4]]
+    Col c -> [(r, c) | r <- [0 .. 4]]
+    MainDiag -> [(i, i) | i <- [0 .. 4]]
+    MinorDiag -> zip [0 .. 4] [4, 3 .. 0]
 
+allComponents :: [Component]
+allComponents = [MainDiag, MinorDiag] ++ [comp | i <- [0 .. 4], comp <- [Row i, Col i]]
+
+allComponentIndices :: [[Idx]]
+allComponentIndices = map componentIndices allComponents
 
 allNums :: [Int]
-allNums = [1..25]
+allNums = [1 .. 25]
 
 -- Generate all component vectors that could be a row, column or diagonal.  These are all distinct sets of 5 numbers
 -- between 1..25 whose sum is 65.
 allVectors :: Array.Array Int ComponentVector
-allVectors = 
-    let vecs = [v | v <- ssolk 5 allNums, sum v == 65]
-        numVecs = length vecs
-    in Array.listArray (0, numVecs - 1) vecs
+allVectors =
+  let vecs = [v | v <- ssolk 5 allNums, sum v == 65]
+      numVecs = length vecs
+   in Array.listArray (0, numVecs - 1) vecs
 
 {-
 For this algorithm, we need to be able to efficiently find all component vectors that contain one or more numbers, for
@@ -56,7 +61,7 @@ one ore more numbers, i.e. all vectors that don't contain 6 or 7. Finally we nee
 i.e. all vectors that contain 1 and 5, but don't contain 6 or 7.
 
 To do this, I'll make two indexes: one that lets me look up the set of vectors containing a number, and another that
-allows looking up the set of all vectors that *don't* contain a number.  Then I can use set operations to get 
+allows looking up the set of all vectors that *don't* contain a number.  Then I can use set operations to get
 combinations of those.
 -}
 
@@ -68,59 +73,60 @@ vectorsByInclude = Map.fromListWith Set.union [(n, Set.singleton i) | (i, v) <- 
 vectorsByExclude :: Map.Map Int Set.IntSet
 vectorsByExclude = Map.fromListWith Set.union [(n, Set.singleton i) | (i, v) <- Array.assocs allVectors, n <- allNums \\ v]
 
-
 -- A function that returns a list of vectors that include includes, and exclude excludes
 -- For example, (filteredVectors [1, 2] [3, 4]) returns all vectors that contain 1 and 2, and don't contain 3 or 4
 filteredVectors :: [Int] -> [Int] -> [ComponentVector]
 filteredVectors includes excludes =
-    let vectorsThatInclude = [vectorsByInclude Map.! i | i <- includes]
-        vectorsThatExclude = [vectorsByExclude Map.! i | i <- excludes]
-        vectors = vectorsThatInclude ++ vectorsThatExclude
-    in 
-        if List.null vectors then 
-            [] 
+  let vectorsThatInclude = [vectorsByInclude Map.! i | i <- includes]
+      vectorsThatExclude = [vectorsByExclude Map.! i | i <- excludes]
+      vectors = vectorsThatInclude ++ vectorsThatExclude
+   in if List.null vectors
+        then []
         else
-            let vecIdxs = Set.toList $ List.foldl1' Set.intersection vectors
-            in map (allVectors !) vecIdxs
+          let vecIdxs = Set.toList $ List.foldl1' Set.intersection vectors
+           in map (allVectors !) vecIdxs
 
 -- Operations on squares
-        
+
 emptySquare :: Square
 emptySquare = Array.listArray ((0, 0), (4, 4)) (repeat 0)
 
 showSquare :: Square -> String
 showSquare square =
-    let ((rs, cs), (re, ce)) = Array.bounds square
-        rows = [unwords [printf "%2d" (square!(r, c)) | c <- [cs..ce]] | r <- [rs..re]]
-    in unlines rows
+  let ((rs, cs), (re, ce)) = Array.bounds square
+      rows = [unwords [printf "%2d" (square ! (r, c)) | c <- [cs .. ce]] | r <- [rs .. re]]
+   in unlines rows
 
+squareIsValid :: Square -> Bool
+squareIsValid square =
+  let compIsValid compIdxs = sum [square ! idx | idx <- compIdxs] == 65
+   in all compIsValid allComponentIndices
 
 -- Assign a row, column or diagonal vector to square.
 -- assignVector startCoord slope vector square
 assignVector :: Component -> ComponentVector -> Square -> Square
 assignVector comp vector square =
-    let idxs = componentIndices comp
-        assocs = zip idxs vector
-    in square Array.// assocs
+  let idxs = componentIndices comp
+      assocs = zip idxs vector
+   in square Array.// assocs
 
-
--- Returns a (vectorIndices, nonZeroValues) tuple for a component, indicating where the 
+-- Returns a (vectorIndices, nonZeroValues) tuple for a component, indicating where the
 -- assigned values for this component are.  nonZeroValues is the list of nonZero values in component,
 -- and vectorIndices are the indices of the non-zeros (0..4) along the component.
 assignedValues :: Component -> Square -> ([Int], [Int])
 assignedValues comp square =
-    let idxs = componentIndices comp
-        nonZeros = [(vecIdx, val) | 
-                    (idx, vecIdx) <- zip idxs [0..], 
-                    let val = square!idx, 
-                    val /= 0]
-    in (map fst nonZeros, map snd nonZeros)
-
+  let idxs = componentIndices comp
+      nonZeros =
+        [ (vecIdx, val)
+          | (idx, vecIdx) <- zip idxs [0 ..],
+            let val = square ! idx,
+            val /= 0
+        ]
+   in (map fst nonZeros, map snd nonZeros)
 
 -- Return a list of all assigned values of a square
 allSquareValues :: Square -> [Int]
 allSquareValues square = filter (/= 0) (Array.elems square)
-
 
 -- Return a list of all permutations of a list, but with only certain elements allowed to move.
 -- For example (vectorPermutations [1,3,4] vec) will return a list of all permutations of vec
@@ -128,100 +134,111 @@ allSquareValues square = filter (/= 0) (Array.elems square)
 -- won't be moved.
 vectorPermutations :: [Int] -> ComponentVector -> [ComponentVector]
 vectorPermutations toMove vector =
-    let enumerated = zip [0..] vector
-        valsToMove = [vector!!i | i <- toMove]
-        permute toMovePermutation =
-            let mapping = zip toMove toMovePermutation
-            in [Maybe.fromMaybe val (List.lookup idx mapping) | (idx, val) <- enumerated]
-    in [permute p | p <- List.permutations valsToMove]
-
+  let enumerated = zip [0 ..] vector
+      valsToMove = [vector !! i | i <- toMove]
+      permute toMovePermutation =
+        let mapping = zip toMove toMovePermutation
+         in [Maybe.fromMaybe val (List.lookup idx mapping) | (idx, val) <- enumerated]
+   in [permute p | p <- List.permutations valsToMove]
 
 -- Given (alignVector indices values vector), where values is a list of values contained in vector, returns
 -- a copy of vector with values moved to be at the corresponding positions in indices.  Assumes all values are
 -- contained in vector, and that indices are in bounds for vector.
 alignVector :: [Int] -> [Int] -> ComponentVector -> ComponentVector
 alignVector indices vals vector =
-    let valAssignments = zip indices vals
-        unassignedIndices = [0..length vector] \\ indices
-        unassignedValues = vector \\ vals
-        -- assign unassigned values to remaining indices
-        otherAssignments = zip unassignedIndices unassignedValues
-        sortedAssignments = List.sortBy (\a b -> compare (fst a) (fst b)) (valAssignments ++ otherAssignments)
-    in map snd sortedAssignments
-
+  let valAssignments = zip indices vals
+      unassignedIndices = [0 .. length vector] \\ indices
+      unassignedValues = vector \\ vals
+      -- assign unassigned values to remaining indices
+      otherAssignments = zip unassignedIndices unassignedValues
+      sortedAssignments = List.sortBy (\a b -> compare (fst a) (fst b)) (valAssignments ++ otherAssignments)
+   in map snd sortedAssignments
 
 centerIndex :: Idx
 centerIndex = (2, 2)
 
 vectorIndices :: [Int]
-vectorIndices = [0..4]
+vectorIndices = [0 .. 4]
 
-
--- Performs one step of the square generation algorithm.  Given an input square, and a component, generates all 
+-- Performs one step of the square generation algorithm.  Given an input square, and a component, generates all
 -- possible new squares resulting from filling in component
 performStep :: Square -> Component -> [Square]
 performStep square component = do
-    let (filledIndices, filledVals) = assignedValues component square
-    let valsToExclude = allSquareValues square \\ filledVals
-    newComponentVec <- filteredVectors filledVals valsToExclude
-    let toMove = vectorIndices \\ filledIndices
-    let alignedVec = alignVector filledIndices filledVals newComponentVec
+  let (filledIndices, filledVals) = assignedValues component square
+  let valsToExclude = allSquareValues square \\ filledVals
+  newComponentVec <- filteredVectors filledVals valsToExclude
+  let toMove = vectorIndices \\ filledIndices
+  let alignedVec = alignVector filledIndices filledVals newComponentVec
 
-    -- iterate over permutations
-    newComponentVec <- vectorPermutations toMove alignedVec
-    return $ assignVector component newComponentVec square
+  -- iterate over permutations
+  newComponentVec <- vectorPermutations toMove alignedVec
+  return $ assignVector component newComponentVec square
 
 {-
+The paper this algorithm is from labels the magic square cells as follows. These labels will be used for some of the
+comments below.
+
 B J K L F
 V C T H X
 R M A P S
 W I U D Y
 G N Q O E
 -}
+
 allSquares :: [Square]
 allSquares = do
-    -- select the center value
-    centerValue <- allNums
-    let centerSquare = emptySquare Array.// [(centerIndex, centerValue)]
+  -- select the center value
+  centerValue <- allNums
+  let centerSquare = emptySquare Array.// [(centerIndex, centerValue)]
 
-    -- select the main diagonal
-    mainDiagSquare <- performStep centerSquare MainDiag
+  -- select the main diagonal
+  mainDiagSquare <- performStep centerSquare MainDiag
 
-    -- Contraint E > B, D > C > B
-    guard (mainDiagSquare!(4,4) > mainDiagSquare!(0,0) &&
-           mainDiagSquare!(3,3) > mainDiagSquare!(1,1) &&
-           mainDiagSquare!(1,1) > mainDiagSquare!(0,0))
+  -- Contraint E > B, D > C > B
+  guard
+    ( mainDiagSquare ! (4, 4) > mainDiagSquare ! (0, 0)
+        && mainDiagSquare ! (3, 3) > mainDiagSquare ! (1, 1)
+        && mainDiagSquare ! (1, 1) > mainDiagSquare ! (0, 0)
+    )
 
-    -- select minor diagonal
-    minorDiagSquare <- performStep mainDiagSquare MinorDiag
+  -- select minor diagonal
+  minorDiagSquare <- performStep mainDiagSquare MinorDiag
 
-    -- constraint I > B, H > B, G > F > B
-    guard (minorDiagSquare!(3,1) > minorDiagSquare!(0,0) &&
-           minorDiagSquare!(1,3) > minorDiagSquare!(0,0) &&
-           minorDiagSquare!(4,0) > minorDiagSquare!(0,4) &&
-           minorDiagSquare!(0,4) > minorDiagSquare!(0,0))
+  -- constraint I > B, H > B, G > F > B
+  guard
+    ( minorDiagSquare ! (3, 1) > minorDiagSquare ! (0, 0)
+        && minorDiagSquare ! (1, 3) > minorDiagSquare ! (0, 0)
+        && minorDiagSquare ! (4, 0) > minorDiagSquare ! (0, 4)
+        && minorDiagSquare ! (0, 4) > minorDiagSquare ! (0, 0)
+    )
 
-    -- select 1st row
-    row0Square <- performStep minorDiagSquare (Row 0)
+  -- select 1st row
+  row0Square <- performStep minorDiagSquare (Row 0)
 
-    -- select 2nd column
-    col1Square <- performStep row0Square (Col 1)
+  -- select 2nd column
+  col1Square <- performStep row0Square (Col 1)
 
-    -- select 4th column
-    col3Square <- performStep col1Square (Col 3)
+  -- select 4th column
+  col3Square <- performStep col1Square (Col 3)
 
-    -- select 5th row
-    row4Square <- performStep col3Square (Row 4)
+  -- select 5th row
+  row4Square <- performStep col3Square (Row 4)
 
-    -- select 3rd row
-    row2Square <- performStep row4Square (Row 2)
+  -- select 3rd row
+  row2Square <- performStep row4Square (Row 2)
 
-    -- select 3rd column
-    col2Square <- performStep row2Square (Col 2)
+  -- select 3rd column
+  col2Square <- performStep row2Square (Col 2)
 
-    -- select 1st column
-    col0Square <- performStep col2Square (Col 0)
+  -- select 1st column
+  col0Square <- performStep col2Square (Col 0)
 
-    -- select 5th column
-    performStep col0Square (Col 4)
+  -- select 5th column
+  col4Square <- performStep col0Square (Col 4)
 
+  guard (squareIsValid col4Square)
+
+  -- For any square whose center is < 13, we can derive another valid square by subtracting each value from 26
+  if col4Square ! centerIndex < 13
+    then [col4Square, fmap (26 -) col4Square]
+    else return col4Square
