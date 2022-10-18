cf3b58fa5bc814bb72cad6940670dce9d7f5308f
ajenkinski <adam@thejenkins.org>
2022-10-18 18:54:07 -0400

    Output message about num threads and chunks

--- app/Main.hs
+++ app/Main.hs
@@ -53,20 +53,23 @@ checkSquares squares =
 
 computeAllParallel :: IO ()
 computeAllParallel = do
-  putStrLn "Starting parallel computation"
+  numThreads <- getNumCapabilities
+  let chunkSize = 10
+  let numChunks = (length Enumerate5x5.allMainDiagSquares + chunkSize - 1) `div` chunkSize
   let squaresPerGroup = map Enumerate5x5.allSquaresForMainDiag Enumerate5x5.allMainDiagSquares
-  let strategy = PS.parListChunk 10 PS.rdeepseq
+  printf "Starting parallel computation with %d threads, and %d chunks\n" numThreads numChunks
+  let strategy = PS.parListChunk chunkSize PS.rdeepseq
   let statsPerGroup = map checkSquares squaresPerGroup `PS.using` strategy
   -- disable buffering, otherwise progress updates without a newline don't get immediately displayed
   SIO.hSetBuffering SIO.stdout SIO.NoBuffering
-  (totalCount, validCount) <-
+  (_, totalCount, validCount) <-
     foldM
-      ( \(total, valid) (total', valid') ->
+      ( \(groupNum, total, valid) (total', valid') ->
           do
             let (newTotal, newNumValid) = (total + total', valid + valid')
-            printf "%15v squares found, %v valid\r" (formatInt newTotal) (formatInt newNumValid)
-            return (newTotal, newNumValid)
+            printf "%d: %v squares found, %v valid\r" groupNum (formatInt newTotal) (formatInt newNumValid)
+            return (groupNum + 1, newTotal, newNumValid)
       )
-      (0, 0)
+      (0 :: Int, 0, 0)
       statsPerGroup
   printf "Total squares found = %d, # valid = %d\n" totalCount validCount
